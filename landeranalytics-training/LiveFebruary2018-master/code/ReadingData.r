4b0060ce87d9d88cb69e903b00d978411c62e9e8
amitanalyste <amitanalyste@gmail.com>
2018-05-12 14:14:57 -0700

    completed read.r

--- landeranalytics-training/LiveFebruary2018-master/code/ReadingData.r
+++ landeranalytics-training/LiveFebruary2018-master/code/ReadingData.r
@@ -1,13 +1,12 @@
 library(readr)
-tomato <- read_csv('data/TomatoFirst.csv')
+tomato <- read_csv("data/TomatoFirst.csv")
+
 tomato
+# tibble or more generic it is a data frame. 
 class(tomato)
 library(readxl)
-
-excel_sheets('data/ExcelExample.xlsx')
-
-tomatoXL <- read_excel('data/ExcelExample.xlsx', sheet='Tomato')
+excel_sheets("data/ExcelExample.xlsx")
+tomatoXL = read_excel("data/ExcelExample.xlsx", sheet="Tomato")
 tomatoXL
-
-wineXL <- read_excel('data/ExcelExample.xlsx', sheet=2)
+wineXL = read_excel("data/ExcelExample.xlsx", sheet = 2)
 wineXL
